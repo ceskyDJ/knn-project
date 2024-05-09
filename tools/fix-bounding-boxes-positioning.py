d7edd3f547dd417179afd2bc52424e82a7ef4257
Michal Šmahel <48548230+ceskyDJ@users.noreply.github.com>
2024-05-09 21:48:28 +0200

    Update script for fixing problems with bounding boxes in raw data

--- tools/fix-bounding-boxes-positioning.py
+++ tools/fix-bounding-boxes-positioning.py
@@ -6,7 +6,7 @@ import pickle
 import re
 import sys
 from collections import defaultdict
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Optional
 
 OFFSETS = defaultdict(lambda: (0, 0), {
     'aha': (-7, 0),
@@ -21,7 +21,7 @@ OFFSETS = defaultdict(lambda: (0, 0), {
     'lidovky': (-6, 0),
     'lupa': (-6, 0),
     'mobilmania': (-6, 0),
-    'pravda': (-6, 0),
+    'pravda': (6, 0),
     'sme': (-6, 0),
     'vtm': (-7, 0),
     'zive': (-6, 0)
@@ -35,17 +35,33 @@ def apply_offset(offset: Tuple[float, float], bounding_box: List[float]):
     return [bounding_box[0] + x, bounding_box[1] + y, bounding_box[2] + x, bounding_box[3] + y]
 
 
-def apply_overlapping_fix(bounding_boxes_value: Dict[str, List[float]]) -> Dict[str, List[float]]:
+def apply_higher_level_fixes(website_name: str, bounding_boxes_value: Dict[str, Optional[List[float]]])\
+        -> Dict[str, Optional[List[float]]]:
     date_bounding_box = bounding_boxes_value['date_published']
     author_bounding_box = bounding_boxes_value['author_name']
 
-    # Fix published date bounding box, when there is author name bounding box in it
-    if author_bounding_box is not None and date_bounding_box is not None:
+    # Zive and child servers:
+    if website_name in ["zive", "doupe", "vtm", "mobilmania", "connect", "avmania"]:
         if (author_bounding_box[0] >= date_bounding_box[0] and author_bounding_box[1] >= date_bounding_box[1]
                 and author_bounding_box[2] <= date_bounding_box[2] and author_bounding_box[3] <= date_bounding_box[3]):
+            # Fix published date bounding box, when there is author name bounding box in it
             date_bounding_box[0] = author_bounding_box[2]
+        else:
+            # Fix published date bounding box, when it is full-width sized, so part of the profile picture is in there
+            date_bounding_box[0] = author_bounding_box[0]
 
-            bounding_boxes_value['date_published'] = date_bounding_box
+    # Pravda: fix published date bounding box, when there is author name bounding box in it
+    if website_name == "pravda":
+        if (author_bounding_box[0] >= date_bounding_box[0] and author_bounding_box[1] >= date_bounding_box[1]
+                and author_bounding_box[2] - 1 <= date_bounding_box[2]
+                and author_bounding_box[3] - 1 <= date_bounding_box[3]):
+            date_bounding_box[1] = author_bounding_box[3]
+
+    # Sme: remove parent references (
+    if website_name == "sme":
+        bounding_boxes_value['parent_reference'] = None
+
+    bounding_boxes_value['date_published'] = date_bounding_box
 
     return bounding_boxes_value
 
@@ -64,7 +80,10 @@ def main():
 
                 for comment_id, comment_bounding_boxes in page_bounding_boxes.items():
                     # Fix known overlapping (of bounding boxes) issues
-                    comment_bounding_boxes: Dict[str, List[float]] = apply_overlapping_fix(comment_bounding_boxes)
+                    comment_bounding_boxes: Dict[str, List[float]] = apply_higher_level_fixes(
+                        website_name,
+                        comment_bounding_boxes
+                    )
 
                     for section_name, bounding_box in comment_bounding_boxes.items():
                         if bounding_box is None:
