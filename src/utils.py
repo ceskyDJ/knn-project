d1b54dad251ffff333698b6c8dfd156ea431840f
Filip Štolfa <phillipstolfa@gmail.com>
2024-05-10 23:50:37 +0200

    feat: concat datasets

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,25 @@
+from pathlib import Path
 import pickle
-from datasets import Dataset
+from datasets import Dataset, concatenate_datasets
 
 
-def load_dataset(filename: str) -> Dataset:
+def load_dataset(filename: str|Path) -> Dataset:
     with open(filename, "rb") as f:
         return pickle.load(f)
+
+
+DS_ROOT = Path("/media/filip/warehouse/fit/knn/datasets/")
+
+def construct_dataset(names: list[str], ds_root=DS_ROOT) -> tuple[Dataset, Dataset]:
+    train_ds = []
+    test_ds = []
+    for n in names:
+        ds = load_dataset(ds_root / f"{n}.pkl")
+
+        nds = ds.train_test_split(test_size=0.2, shuffle=True)
+
+        train_ds.append(nds["train"])
+        test_ds.append(nds["test"])
+
+
+    return concatenate_datasets(train_ds), concatenate_datasets(test_ds)
