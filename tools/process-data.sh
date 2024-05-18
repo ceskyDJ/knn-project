2d70e61fca65c4cfef0ddb861463f3a79b22ec11
Jozef Cabala <jozef.cabala.ml@gmail.com>
2024-05-19 01:24:48 +0200

    fixed bug in process-data.sh

--- tools/process-data.sh
+++ tools/process-data.sh
@@ -25,7 +25,6 @@ sites=(
   "zive"
 )
 
-# Loop through each site and run the command
 for site in "${sites[@]}"; do
-  nohup ./miniforge3/bin/conda run -n knn python src/process_dataset.py --sites "$site" & | tee "log-$(date +"%Y-%m-%d")-[$site]-v4-split.txt"
+  nohup ./miniforge3/bin/conda run -n knn python src/process_dataset.py --sites "$site" > "log-$(date +"%Y-%m-%d")-[$site]-v4-split.txt" 2>&1 &
 done
